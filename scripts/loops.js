4342af2d232aa500e7eac0297c0fb7a3a3cbd64e
Chan Pham <tqp2001@columbia.edu>
2025-05-02 19:49:43 +0000

    loop backend feature

---
+++ scripts/loops.js
@@ -0,0 +1,27 @@
+const DARK_COLOR = "#073B3A"
+
+function clearLight(){
+    for (let i = 0; i < lightbulbs.length; i++){
+        lightbulbs[i].style.backgroundColor = DARK_COLOR
+    }
+}
+
+let lightbulbs = document.querySelectorAll(".lightbulb")
+let runButton = document.querySelector("#run")
+let init = document.querySelector("#init")
+let stop = document.querySelector("#stop")
+let inc = document.querySelector("#inc")
+
+runButton.addEventListener("click", ()=>{
+    clearLight()
+    
+    let initNum = parseInt(init.value)
+    let stopNum = parseInt(stop.value)
+    let incNum = parseInt(inc.value)
+
+    console.log(`let i = ${initNum}; i < ${stopNum}; i += ${incNum}`)
+    for (let i = initNum; i < stopNum; i += incNum){
+        console.log("turning on " + i)
+        lightbulbs[i].style.backgroundColor = "white"
+    }
+})
